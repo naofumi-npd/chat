12d5ee4e9c4c8c8fbd56fd0feb531ac7c4125816
Naofumi Ezaki <Naofumi.Ezaky@ey.com>
2015-12-22 10:14:15 -0500

    update

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 var express = require("express");
 var socket = require('socket.io');
+var routes = require('./routes/index');
+
+
 var app = express();
 var port = 3700;
 
-app.set('views', __dirname + '/tpl');
+app.set('views', __dirname + '/views');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
 app.use(express.static(__dirname + '/public'));
 
-app.get("/", function(req, res){
-    res.render("page");
-});
-
+app.use('/', routes);
  
 var io = socket.listen(app.listen(port));
 
